17e439e575bad97f68b3748628535912026b17ad
Mustafa Siddiqui <mustafa.a.r.siddiqui@outlook.com>
2024-12-02 07:40:58 -0500

    task/ started working on endpoint for user registration

--- internal/handlers/users.go
+++ internal/handlers/users.go
@@ -6,6 +6,21 @@ import (
 	"time"
 )
 
+type User struct {
+	Username  string    `json:"username"`
+	Password  string    `json:"-"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+// var (
+// 	users      = make(map[string]User)
+// 	usersMutex = sync.Mutex{}
+// )
+
+func RegisterUser(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func CheckHealth(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprintf(w, "Server is up and running - %v", time.Now())
